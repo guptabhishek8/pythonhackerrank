eec6d382a9752fce2fd2a31b6f98d6c96d0c60bc
Abhishek <guptabhishek@gmail.com>
2019-09-02 15:27:15 +0530

    question1

---
+++ 1/1.py
@@ -0,0 +1,14 @@
+if __name__ == '__main__':
+    n = int(input())
+    student_marks = {}
+    for _ in range(n):
+        name, *line = input().split()
+        scores = list(map(float, line))
+        student_marks[name] = scores
+    query_name = input()
+    b=0
+    c=0
+    for a in student_marks[query_name]:
+        b=b+a
+        c+=1
+    print(format(b/c , '.2f'))
